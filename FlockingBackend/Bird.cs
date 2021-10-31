662cd176f600bbd9d56b032522b694e80c33ac38
Nael Louis <nael.louis1002@gmail.com>
2021-10-31 19:24:30 -0400

    Modify method header

--- FlockingBackend/Bird.cs
+++ FlockingBackend/Bird.cs
@@ -1,74 +1,81 @@
 using System;
+using System.Collections.Generic;
 
-namespace FlockingBackend{
-    public abstract class Bird{
+namespace FlockingBackend
+{
+    public abstract class Bird
+    {
 
         //Position property to get and set the position of the bird
-        public Vector2 Position{
+        public Vector2 Position
+        {
             get;
             protected set;
         }
 
         //Velocity property to get and set the position of the bird
-        public Vector2 Velocity{
+        public Vector2 Velocity
+        {
             get;
             protected set;
         }
         //AmountToSteer variable used to make the birds follow other birds
-        protected Vector2 amountToSteer;    
+        protected Vector2 amountToSteer;
 
         //Bird constructor that puts the bird in a random positon and gives it a random velocity.
         //Also sets amount to steer to 0
-        public Bird(){
+        public Bird()
+        {
             Random rnd = new Random();
-            int Px = rnd.Next(0,World.Width);
-            int Py = rnd.Next(0,World.Height);
-            Position = new Vector2(Px,Py);
-            int VelX = rnd.Next(0,9)-4;
-            int VelY = rnd.Next(0,9)-4;
-            Velocity = new Vector2(VelX,VelY);
-            amountToSteer = new Vector2(0,0);
+            int Px = rnd.Next(0, World.Width);
+            int Py = rnd.Next(0, World.Height);
+            Position = new Vector2(Px, Py);
+            int VelX = rnd.Next(0, 9) - 4;
+            int VelY = rnd.Next(0, 9) - 4;
+            Velocity = new Vector2(VelX, VelY);
+            amountToSteer = new Vector2(0, 0);
         }
-        
+
         //another bird constructor used for testing purposes
-        public Bird(float P1, float P2 , float V1 , float V2){
-            Position = new Vector2(P1,P2);
-            Velocity = new Vector2(V1,V2);
-            amountToSteer = new Vector2(0,0);
+        public Bird(float P1, float P2, float V1, float V2)
+        {
+            Position = new Vector2(P1, P2);
+            Velocity = new Vector2(V1, V2);
+            amountToSteer = new Vector2(0, 0);
         }
 
         ///<value> Property <c>Rotation</c> to rotate the raven to face the direction it is moving toward.</value>
         public float Rotation
         {
-            get 
+            get
             {
-                return (float)Math.Atan2(this.Velocity.Vy, this.Velocity.Vx); 
+                return (float)Math.Atan2(this.Velocity.Vy, this.Velocity.Vx);
             }
         }
 
         ///<summary>
         ///This method is a private helper method to make sparrows reappear on the opposite edge if they go outside the bounds of the screen
         ///</summary>
-       private void AppearOnOppositeSide()
-       {
-           if (this.Position.Vx > World.Width)
+        private void AppearOnOppositeSide()
+        {
+            if (this.Position.Vx > World.Width)
             {
                 this.Position = new Vector2(0, this.Position.Vy);
             }
-            else if(this.Position.Vx < 0)
+            else if (this.Position.Vx < 0)
             {
-                 this.Position = new Vector2(World.Width, this.Position.Vy);
+                this.Position = new Vector2(World.Width, this.Position.Vy);
             }
             if (this.Position.Vy > World.Height)
             {
                 this.Position = new Vector2(this.Position.Vy, 0);
             }
-            else if(this.Position.Vy < 0)
+            else if (this.Position.Vy < 0)
             {
-                this.Position= new Vector2(this.Position.Vx, World.Height);
+                this.Position = new Vector2(this.Position.Vx, World.Height);
             }
-       }
+        }
 
-       public abstract Vector2 CalculateBehaviour(List<Sparrow> Sparrows);
+        public abstract void CalculateBehaviour(List<Sparrow> Sparrows);
     }
 }
