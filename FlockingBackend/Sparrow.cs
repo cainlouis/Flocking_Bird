a83ccbe98b19649887f7da2ab3e838df76d6ee05
Nael Louis <nael.louis1002@gmail.com>
2021-11-03 09:16:52 -0400

    Finish Sparrow - before tests

--- FlockingBackend/Sparrow.cs
+++ FlockingBackend/Sparrow.cs
@@ -14,7 +14,7 @@ namespace FlockingBackend
         {
         }
 
-        public Sparrow(float P1, float P2, float V1, float V2) : base()
+        public Sparrow(float P1, float P2, float V1, float V2) : base(P1, P2, V1, V2)
         {
         }
 
@@ -24,8 +24,6 @@ namespace FlockingBackend
         ///<param name="sparrows">List of sparrows</param>
         public override void CalculateBehaviour(List<Sparrow> sparrows)
         {
-            //TODO: Set the amountToSteer vector with the vectors returned by 
-            //Cohesion, Alignment, Avoidance methods
             Vector2 alignmentAndCohesion = Alignment(sparrows) + Cohesion(sparrows);
             Vector2 sum = Avoidance(sparrows) + alignmentAndCohesion;
             amountToSteer += sum;
@@ -37,7 +35,6 @@ namespace FlockingBackend
         ///<param name="raven">A Raven object</param>
         public void CalculateRavenAvoidance(Raven raven)
         {
-            //TODO: Add the vector returned by FleeRaven to the amountToSteer vector.
             amountToSteer += FleeRaven(raven);
         }
 
@@ -45,11 +42,88 @@ namespace FlockingBackend
         //The method headers are declared below:
         private Vector2 Alignment(List<Sparrow> sparrows)
         {
-
+            Vector2 align = new Vector2(0,0);
+            List<Sparrow> neighbours = GetNeighbours(sparrows);
+            if (neighbours != null)
+            {
+                foreach (Sparrow sparrow in neighbours)
+                {
+                    align += sparrow.Velocity;
+                }
+                GetVector2(align, neighbours.Count, "alignment");
+            }
+            return align;
         }
-        private Vector2 Cohesion(List<Sparrow> sparrows);
-        private Vector2 Avoidance(List<Sparrow> sparrows);
-        private Vector2 FleeRaven(Raven raven);
+
+        private Vector2 Cohesion(List<Sparrow> sparrows) {
+            Vector2 displacement = new Vector2(0, 0);
+            List<Sparrow> neighbours = GetNeighbours(sparrows);
+            if (neighbours != null)
+            {
+                foreach (Sparrow sparrow in sparrows)
+                {
+                    displacement += sparrow.Position;
+                }
+                displacement = GetVector2(displacement, neighbours.Count, "cohesion");
+            }
+            return displacement;
+        }
+        private Vector2 Avoidance(List<Sparrow> sparrows) {
+            float distance;
+            int radius = World.AvoidanceRadius; 
+            Vector2 difference;
+            Vector2 result = new Vector2(0,0);
+            int count = 0;
+            foreach (Sparrow sparrow in sparrows) {
+               distance = Vector2.DistanceSquared(this.Position, sparrow.Position);
+               if (distance < Math.Pow(radius, 2) && !this.Equals(sparrow)) {
+                   difference = this.Position - sparrow.Position;
+                   difference /= distance;
+                   result += difference; 
+                   count++;
+               }
+            }
+            if (count > 0) {
+                result = GetVector2(result, count, "avoidance");
+            }
+            return result;
+        }
+        private Vector2 FleeRaven(Raven raven)
+        {
+            Vector2 toSteer = new Vector2(0,0);
+            float distance = Vector2.DistanceSquared(this.Position, raven.Position);
+            if (distance < Math.Pow(World.AvoidanceRadius, 2)) {
+                toSteer = this.Position - raven.Position;
+                toSteer /= distance;
+                toSteer = Vector2.Normalize(toSteer) * World.MaxSpeed;
+            }
+            return toSteer;
+        }
+
+        private List<Sparrow> GetNeighbours(List<Sparrow> sparrows) {
+            List<Sparrow> neighbours = new List<Sparrow>();
+            int radius = World.NeighbourRadius;
+            float distance;
+            foreach (Sparrow sparrow in sparrows) {
+               distance = Vector2.DistanceSquared(this.Position, sparrow.Position);
+               if (distance < Math.Pow(radius, 2) && !this.Equals(sparrow)) {
+                   neighbours.Add(sparrow);
+               }
+            }
+            return neighbours;
+        }
+
+        private Vector2 GetVector2(Vector2 toModify, int count, string whichFunction) {
+            toModify = toModify / count;
+            if (whichFunction.Equals("cohesion")) {
+               toModify = Vector2.Normalize(toModify);
+               toModify -= this.Position; 
+            }
+            toModify = Vector2.Normalize(toModify) * World.MaxSpeed;
+            toModify -= this.Velocity;
+            toModify = Vector2.Normalize(toModify);
+            return toModify;
+        }
 
     }
 }
