29d08da262d7dacda13991590a8f9e9f8d79dd51
Nael Louis <nael.louis1002@gmail.com>
2021-11-09 11:14:35 -0500

    Add ChaseSparrow body and CalculateBehaviour

--- FlockingBackend/Raven.cs
+++ FlockingBackend/Raven.cs
@@ -17,6 +17,9 @@ namespace FlockingBackend
         public void Move()
         {
             //TODO: 
+            this.Velocity += amountToSteer;
+            this.Velocity /= World.MaxSpeed;
+            this.Position += this.Velocity;
         }
         ///<summary>
         ///This method is an event handler to calculate and set amountToSteer vector
@@ -24,12 +27,27 @@ namespace FlockingBackend
         ///<param name="sparrows">List of sparrows</param>
         public override void CalculateBehaviour(List<Sparrow> sparrows) 
         {
-            //TODO: Set the amountToSteer vector with the vector returned by the ChaseSparrow.
+            amountToSteer = ChaseSparrow(sparrows);
         }
 
         //TODO: Code the following private helper methods to implement chase sparrows.
         //The method header are declared below:
-        private Vector2 ChaseSparrow (List<Sparrow> sparrows);
-        
+        private Vector2 ChaseSparrow (List<Sparrow> sparrows) {
+            float smallestDistance= World.AvoidanceRadius;
+            float distance;
+            Sparrow closestSparrow = null;
+            foreach (Sparrow sparrow in sparrows) {
+                distance = Vector2.DistanceSquared(sparrow.Position, this.Position);
+                if (distance < smallestDistance){
+                    smallestDistance = distance;
+                    closestSparrow = sparrow;
+                }
+            }
+            Vector2 result = new Vector2(0,0);
+            if (closestSparrow != null) {
+                result = closestSparrow.Position - this.Position;
+            }
+            return result;
+        }  
     }
 }
