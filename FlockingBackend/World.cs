58b8f38c5d8e40cd3f7b84316ddf92d65c4ef928
Nael Louis <nael.louis1002@gmail.com>
2021-11-12 17:12:35 -0500

    Change uppercase for move

--- FlockingBackend/World.cs
+++ FlockingBackend/World.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 
-namespace FlockingBackend{
-    public abstract class World{
+namespace FlockingBackend
+{
+    public abstract class World
+    {
         private Flock flock;
-        public List<Sparrow> sparrows{
+        public List<Sparrow> sparrows
+        {
             get;
         }
-        public Raven raven{
+        public Raven raven
+        {
             get;
         }
         public static int InitialCount; //number of sparrows
@@ -17,7 +21,8 @@ namespace FlockingBackend{
         public static int NeighbourRadius; //Radius used to determine if a bird is a neighbour
         public static int AvoidanceRadius; //Radius used to determine if a bird is too close
 
-        static World(){
+        static World()
+        {
             InitialCount = 150; //number of sparrows
             Width = 1000; //Width of the canvas (“world”)
             Height = 500; //Height of the canvas
@@ -31,26 +36,29 @@ namespace FlockingBackend{
         ///This method is a parameterless constructor that creates the flock and adds the sparrows to the list
         //as well as subscribing them to the events.
         ///</summary>
-        public World(){
+        public World()
+        {
             flock = new Flock();
             //create sparrow list
             sparrows = new List<Sparrow>();
             //create sparrows and add them to the list
-            for (int i = 0; i < InitialCount; i++){
+            for (int i = 0; i < InitialCount; i++)
+            {
                 Sparrow sparrow = new Sparrow();
                 sparrows.Add(sparrow);
                 //subscribe the flock
-                flock.Subscribe(sparrow.CalculateBehaviour,sparrow.move,sparrow.CalculateRavenAvoidance);
-            } 
+                flock.Subscribe(sparrow.CalculateBehaviour, sparrow.Move, sparrow.CalculateRavenAvoidance);
+            }
             //create a raven and subscribe it to the flock
             raven = new Raven();
-            flock.Subscribe(raven.CalculateBehaviour,raven.move);   
+            flock.Subscribe(raven.CalculateBehaviour, raven.Move);
         }
         ///<summary>
         ///This method raises the moe events that move the raven and the sparrow
         ///</summary>
-        public void Update(){
-            flock.RaiseMoveEvents(this.sparrows,this.raven);
+        public void Update()
+        {
+            flock.RaiseMoveEvents(this.sparrows, this.raven);
         }
 
     }
