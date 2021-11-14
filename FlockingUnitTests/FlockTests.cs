c2e1de71528c8a956bf7035c8d1666cab73e4c5c
Christian Bula <oleg.bula@dawsoncollege.qc.ca>
2021-11-14 14:51:43 -0500

    added flock test

--- FlockingUnitTests/FlockTests.cs
+++ FlockingUnitTests/FlockTests.cs
@@ -10,29 +10,65 @@ namespace FlockingUnitTests
         [TestMethod]
         public void TestSubscribeSparrow()
         {
+            //arange
             Flock flock = new Flock();
-
             List<Sparrow> sparrows = new List<Sparrow>();
 
-            Sparrow s1 = new Sparrow( 6, 6, -3,4 );
-            flock.Subscribe(s1.CalculateBehaviour,s1.Move,s1.CalculateRavenAvoidance);
-            Sparrow s2 = new Sparrow( 2, 4, -2,1 );
-            flock.Subscribe(s2.CalculateBehaviour,s2.Move,s2.CalculateRavenAvoidance);
+            Sparrow sparrow1 = new Sparrow( 10, 6, 3,3 );
+            Sparrow sparrow2 = new Sparrow( 1, 2, 1,2 );
+            Sparrow sparrow3 = new Sparrow( 4, 5, 5,-2 );
 
-            sparrows.Add(s1);
-            sparrows.Add(s2);
+            flock.Subscribe(sparrow1.CalculateBehaviour,sparrow1.Move,sparrow1.CalculateRavenAvoidance);
+            flock.Subscribe(sparrow2.CalculateBehaviour,sparrow2.Move,sparrow2.CalculateRavenAvoidance);
+            flock.Subscribe(sparrow3.CalculateBehaviour,sparrow3.Move,sparrow3.CalculateRavenAvoidance);
+
+            sparrows.Add(sparrow1);
+            sparrows.Add(sparrow2);
+            sparrows.Add(sparrow3);
        
             Raven raven = new Raven();
 
-            float expX = -2.31f;
-            float expY = 1.722f;
+            float velocityXresult = (float)2.8716412;
+            float velocityYresult = (float)0.60421896;
             
             // Act
             flock.RaiseMoveEvents(sparrows,raven);
             
             //Assert
-            Assert.AreEqual(expX, s1.Velocity.Vx, 0.01);
-            Assert.AreEqual(expY, s1.Velocity.Vy, 0.01);
+            Assert.AreEqual(velocityXresult, sparrow1.Velocity.Vx);
+            Assert.AreEqual(velocityYresult, sparrow1.Velocity.Vy);
+        }
+
+        [TestMethod]
+        public void TestSubscribeSparrowNoNeighbor()
+        {
+            //arange
+            Flock flock = new Flock();
+            List<Sparrow> sparrows = new List<Sparrow>();
+
+            Sparrow sparrow1 = new Sparrow( 200, 200, 3,3 );
+            Sparrow sparrow2 = new Sparrow( 1, 2, 1,2 );
+            Sparrow sparrow3 = new Sparrow( 4, 5, 5,-2 );
+
+            flock.Subscribe(sparrow1.CalculateBehaviour,sparrow1.Move,sparrow1.CalculateRavenAvoidance);
+            flock.Subscribe(sparrow2.CalculateBehaviour,sparrow2.Move,sparrow2.CalculateRavenAvoidance);
+            flock.Subscribe(sparrow3.CalculateBehaviour,sparrow3.Move,sparrow3.CalculateRavenAvoidance);
+
+            sparrows.Add(sparrow1);
+            sparrows.Add(sparrow2);
+            sparrows.Add(sparrow3);
+       
+            Raven raven = new Raven();
+
+            float velocityXresult = (float)3;
+            float velocityYresult = (float)3;
+            
+            // Act 
+            flock.RaiseMoveEvents(sparrows,raven);
+            
+            //Assert
+            Assert.AreEqual(velocityXresult, sparrow1.Velocity.Vx);
+            Assert.AreEqual(velocityYresult, sparrow1.Velocity.Vy);
         }
     }
 }
